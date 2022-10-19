baffcac5ca20cb59ac266ab8cb3a6c244181b79e
elmarti <jerpdertin@gmail.com>
2022-10-19 10:25:13 +0100

    :bug: don't exit

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ if [ -z "${FIREBASE_PROJECT}" ]; then
 fi
 if [ -z "${FIREBASE_ENVIRONMENT}" ]; then
     firebase use ${FIREBASE_ENVIRONMENT} --project ${FIREBASE_PROJECT}
-    exit 1
 fi
 if [ -z "${FIREBASE_ONLY_HOSTING}" == "true" ]; then
     firebase deploy \
